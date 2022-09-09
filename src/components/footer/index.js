536dabcac5804355db64c51d5ddf1bf5a709a5f0
Tien Lam <tienlam130894@gmail.com>
2022-09-09 13:19:46 +0700

    Init project

---
+++ src/components/footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Container from '../container';
+import './index.css';
+
+const Footer = () => {
+  return <div className='footer-container'>
+    <Container>
+      <div>Footer</div>
+    </Container>
+  </div>
+}
+
+export default Footer;
---
+++ src/components/header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Container from '../container';
+import './index.css';
+
+const Header = () => {
+  return (
+    <div className='header-container'>
+      <Container>
+        <div>Header</div>
+      </Container>
+    </div>
+  );
+}
+
+export default Header;
