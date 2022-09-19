645bb12a1f57a956159f136181425e11942df982
Tien Lam <tienlam130894@gmail.com>
2022-09-19 15:16:35 +0700

    update 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
-import './App.css';
-import Header from './components/header';
-import TopicList from './components/topicList';
-import React, { useState} from 'react';
-import { TOPICS } from './components/topicList/data';
-
-function App() {
-  const [topics, setTopics] = useState(TOPICS);
-  const [showFavoriteList, setShowFavoriteList] = useState(false);
-  return (
-    <div className="App">
-      <Header
-        topics={topics}
-        onShowFavoriteList={() => setShowFavoriteList(!showFavoriteList)}
-      />
-      <div>
-        <TopicList
-          topics={topics}
-          setTopics={setTopics}
-        />
-      </div>
-    </div>
-  );
+class Aninmal {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  sound() {
+  }
 }
-
-export default App;
